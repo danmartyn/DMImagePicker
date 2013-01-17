f4c4a77d197b38ef7d37b33e161ba7e4f65cf2d8
Daniel Martyn <danmartyn@gmail.com>
2013-01-17 10:26:09 -0500

    Initial Commit

---
+++ DMImagePicker/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  DMImagePicker
+//
+//  Created by Daniel Martyn on 2013-01-17.
+//  Copyright (c) 2013 Dan Martyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DMImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DMImagePicker
+//
+//  Created by Daniel Martyn on 2013-01-17.
+//  Copyright (c) 2013 Dan Martyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+    }
+}
